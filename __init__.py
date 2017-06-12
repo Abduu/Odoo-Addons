d5a8059eb3e7e99dc0334de37f9ca1aa48a16a95
Abdelkader <sallemine.abdelkader@gmail.com>
2017-06-12 05:45:49 -0700

    V1 init file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+class ClassName(Delcho):
+	"""docstring for ClassName"""
+	def __init__(self,Dekchii,daddzf,ergeg):
+		super(ClassName, self).__init__()
+		self.Dekchii = Dekchii
+		self.daddzf = daddzf
+		self.ergeg = ergeg
+		
